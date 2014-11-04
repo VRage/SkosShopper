99c1e0364810691f53d5fee448599848dbb1fc74
vrage <vrage@vrage-PC>
2014-11-04 10:59:01 +0100

    added log4j.properties also implemented Logger for main class   PLS read examples for logpriority in log4j.properties file

--- SkosShopper/src/application/Main.java
+++ SkosShopper/src/application/Main.java
@@ -1,5 +1,9 @@
 package application;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.jmx.LoggerDynamicMBean;
+import org.slf4j.impl.Log4jLoggerFactory;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +13,8 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	public static final Logger log = Logger.getLogger(Main.class);
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,8 +23,9 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			log.info("app started without errors");
 		} catch(Exception e) {
-			e.printStackTrace();
+			log.error(e, e);
 		}
 	}
 	
