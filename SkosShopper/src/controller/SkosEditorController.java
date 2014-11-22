1f19dfd11bf560c2d3024152e31eac1a99746e38
vrage <vrage@vrage-PC>
2014-11-22 12:30:16 +0100

    added views to edit Label

--- SkosShopper/src/controller/SkosEditorController.java
+++ SkosShopper/src/controller/SkosEditorController.java
@@ -77,7 +77,10 @@ public class SkosEditorController implements Initializable {
 	private TextField txtfield_individiaulname;
 	@FXML
 	private TextField txtfield_IndiLabel;
-	
+	@FXML
+	private TextField txtfield_editLabel;
+	@FXML
+	private Button btn_editLabel;
 	
 	// local j4log logger
 	public static final Logger log = Logger
@@ -101,7 +104,7 @@ public class SkosEditorController implements Initializable {
 	
 	//In this class used Ontology Model
 	private OntModel model = ModelFactory
-			.createOntologyModel( OntModelSpec.OWL_MEM);
+			.createOntologyModel( OntModelSpec.OWL_MEM_TRANS_INF);
 	
 	//Selected OntClass and Individual
 	private OntClass selectedOntClass;
